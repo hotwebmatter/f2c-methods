9f5e031f5215f35f0297da55dac9159a8dae1f9f
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-18 16:53:48 -0500

    Read Fahrenheit temp from user input

--- Program.cs
+++ Program.cs
@@ -5,8 +5,14 @@ namespace f2c_methods
 {
     class Program
     {
+        // no class-level static variables this time
         static void Main(string[] args)
         {
+            // initialize variables in Main()
+            double tempFahrenheit,
+                   tempCelsius;
+            tempFahrenheit = ReadDouble("Enter temperature in degrees Fahrenheit:");
+            tempCelsius = f2c(tempFahrenheit);
             WriteLine("Hello World!");
         }
 
@@ -14,6 +20,17 @@ namespace f2c_methods
         {
             return 5 * (f - 32) / 9;
         }
-
+        static double ReadDouble(string label)
+        {
+            Write("{0} ", label);
+            double number;
+            string userInput = ReadLine();
+            while (Double.TryParse(userInput, out number) == false)
+            {
+                Write("{0} {1} ", "Invalid input!", label);
+                userInput = ReadLine();
+            }
+            return number;
+        }
     }
 }
