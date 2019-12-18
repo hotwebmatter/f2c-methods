e0e2fab7879a0527b0a26a2b446ae4057e9c9a8a
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-18 16:36:59 -0500

    Initial commit of f2c-methods project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace f2c_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
